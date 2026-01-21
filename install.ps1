2750748575500c88a46a98fd4ef3c91ed4db68e3
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2026-01-21 15:11:11 -0800

    Delete install.ps1

--- install.ps1
+++
@@ -1,6 +0,0 @@
-Install-product node 6.2.0
-node --version
-cmd /c "npm install jspm@0.16.54 -g --depth 0"
-cmd /c "npm install --depth 0"
-jspm config registries.github.auth %JSPM_GITHUB_AUTH_TOKEN%
-jspm install -y
